ec181a5904ec10a9280c2bae5e6b8088cda02695
Anita Yang <amyang@student.unimelb.edu.au>
2023-05-19 21:34:59 +1000

    Added initial portal travel logic (not yet integrated)

--- pacman/src/PortalPair.java
+++ pacman/src/PortalPair.java
@@ -3,12 +3,16 @@ package src;
 // TODO: Added PortalPair class for adding portals to map,
 //  and also additional functions for level checking purposes
 
+import ch.aplu.jgamegrid.*;
+
 public class PortalPair {
     // Each portal is an Item
     private Item portal1;
     private Item portal2;
-
     private int countPortals;
+    // Flag to check the nature of PacMan-Portal overlap
+    // (i.e., did PacMan move onto Portal, or was PacMan transported onto Portal?)
+    private boolean movedOntoPortal = true;
     public final static int VALID_PAIR = 2;
     public final static int EMPTY_PAIR = 0;
 
@@ -50,5 +54,32 @@ public class PortalPair {
      */
     private boolean checkTwoDistinctTiles(){
         return !(portal1.getLocation().equals(portal2.getLocation()));
+    }
+
+    /**
+     * Function to transport PacMan to the other portal
+     */
+    public Location movePacMan(PacActor pacActor){
+        // If PacMan overlapped with portal #1, move PacMan to portal #2
+        if (portal1.getLocation().equals(pacActor.getLocation()) && movedOntoPortal){
+            movedOntoPortal = false;
+            return portal2.getLocation();
+        }
+        // If PacMan overlapped with portal #2, move PacMan to portal #1
+        else if (portal2.getLocation().equals(pacActor.getLocation()) && movedOntoPortal){
+            movedOntoPortal = false;
+            return portal1.getLocation();
+        }
+        return null;
+    }
+
+    /**
+     * Function to reset movedOntoPortal flag, so that PacMan can repeatedly travel via portals
+     */
+    public void setMovedOntoPortal(PacActor pacActor){
+        if (!(portal1.getLocation().equals(pacActor.getLocation()) &&
+                portal2.getLocation().equals(pacActor.getLocation()))){
+            movedOntoPortal = true;
+        }
     }
 }
